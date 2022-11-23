46aaa8f289654bd5c9d36e3b757f9e568775fccf
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-11-23 16:17:24 -0300

    Send Reset Password ok

--- ProjectWebApp/settings.py
+++ ProjectWebApp/settings.py
@@ -135,3 +135,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # It is the public url of the directory
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'desarrollo.ppagano91@gmail.com'
+EMAIL_HOST_PASSWORD='iwrjpsqxygqiclcs'
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
