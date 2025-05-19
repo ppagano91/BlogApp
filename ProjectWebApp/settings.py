6b49cc60ee74285969b46424ebbf95186baad0b4
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2025-05-19 10:42:15 -0300

    Actualización de Variables de Entorno

--- ProjectWebApp/settings.py
+++ ProjectWebApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r5oz@$5myz-)u+!@igsjket7t9lmgm07q7tuv2qq-*nu5r$6m&'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
